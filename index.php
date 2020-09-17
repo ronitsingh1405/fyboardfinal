774ae738cf2a75c1c850a75d1c6bf8ede3d1e80e
mysimplecs <71099709+mysimplecs@users.noreply.github.com>
2020-09-17 19:07:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- ===== CSS ===== -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        
+        <title>FY Board - NYUAD</title>
+    </head>
+    <body id="body-pd">
+        <div class="container">
+
+            <div class="row">
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+            </div>
+      
+            <div class="row altline2">
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+            </div>
+      
+            <div class="row line2">
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+            </div>
+      
+            <div class="row altline3">
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+            </div>
+      
+            <div class="row line3">
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+            </div>
+      
+            <div class="row altline4">
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+            </div>
+      
+            <div class="row line4">
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+            </div>
+      
+            <div class="row altline5">
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+            </div>
+      
+            <div class="row line5">
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+            </div>
+      
+            <div class="row">
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+            </div>
+      
+            <div class="row">
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+            </div>
+      
+            <div class="row">
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+              <div class="hexagon">  </div>
+            </div>
+          </div>
+        <div class="l-navbar" id="navbar">
+            <nav class="nav">
+                <div>
+                    <div class="nav__brand">
+                        <ion-icon name="menu-outline" class="nav__toggle" id="nav-toggle"></ion-icon>
+                        <a href="index.html" class="nav__logo">FY Board</a>
+                    </div>
+                    <div class="nav__list">
+                        <a href="index.html" class="nav__link active">
+                            <ion-icon name="home-outline" class="nav__icon"></ion-icon>
+                            <span class="nav__name">Home</span>
+                        </a>
+                        <a href="today.html" class="nav__link">
+                            <ion-icon name="checkmark-circle-outline" class="nav__icon"></ion-icon>
+                            <span class="nav__name">Today's Events</span>
+                        </a>
+                        <a href="reminders.html" class="nav__link">
+                          <ion-icon name="alarm" class="nav__icon"></ion-icon>
+                          <span class="nav__name">Reminders</span>
+                      </a>
+                      <a href="deadlines.html" class="nav__link">
+                        <ion-icon name="clipboard-outline" class="nav__icon"></ion-icon>
+                        <span class="nav__name">Deadlines</span>
+                    </a>
+                      <a href="sig.html" class="nav__link">
+                        <ion-icon name="color-filter-outline" class="nav__icon"></ion-icon>
+                        <span class="nav__name">SIG Events</span>
+                    </a>
+<!-- 
+                        <div  class="nav__link collapse">
+                            <ion-icon name="folder-outline" class="nav__icon"></ion-icon>
+                            <span class="nav__name">Projects</span>
+
+                            <ion-icon name="chevron-down-outline" class="collapse__link"></ion-icon>
+
+                            <ul class="collapse__menu">
+                                <a href="#" class="collapse__sublink">Data</a>
+                                <a href="#" class="collapse__sublink">Group</a>
+                                <a href="#" class="collapse__sublink">Members</a>
+                            </ul>
+                        </div>
+                        chatbubbles-outline
+-->
+                        <a href="weblinks.html" class="nav__link">
+                            <ion-icon name="globe" class="nav__icon"></ion-icon>
+                            <span class="nav__name">Website Links</span>
+                        </a>
+                        
+                        <!-- 
+                        <div class="nav__link collapse">
+                            <ion-icon name="people-outline" class="nav__icon"></ion-icon>
+                            <span class="nav__name">Team</span>
+
+                            <ion-icon name="chevron-down-outline" class="collapse__link"></ion-icon>
+
+                            <ul class="collapse__menu">
+                                <a href="#" class="collapse__sublink">Data</a>
+                                <a href="#" class="collapse__sublink">Group</a>
+                                <a href="#" class="collapse__sublink">Members</a>
+                            </ul>
+                        </div> -->
+                        <a href="whatsapp.html" class="nav__link">
+                            <ion-icon name="alert" class="nav__icon"></ion-icon>
+                            <span class="nav__name">WhatsApp Links</span>
+                        </a>
+                    </div>
+                </div>
+
+                <a href="NYUAD Freshman Noticeboard.pdf" target="_blank" class="nav__link">
+                    <ion-icon name="download-outline" class="nav__icon"></ion-icon>
+                    <span class="nav__name">Download PDF</span>
+                </a>
+            </nav>
+        </div>
+
+
+        <!-- ===== IONICONS ===== -->
+        <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
+        
+        <!-- ===== MAIN JS ===== -->
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
